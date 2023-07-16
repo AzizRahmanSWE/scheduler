9b54f15d98211fb2bbd28c23a0bc860fd530461b
Friies <bahram.parsa2019@gmail.com>
2023-07-16 14:38:01 -0400

    Build the InterviewereListItem & InterviewerList. - replace hard-coded value with props. - Add conditional css classes to render interviewers names.

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -4,7 +4,6 @@ import "components/DayListItem.scss";
 
 export default function DayListItem(props) {
 
-
   const dayClass = classNames("day-list__item", {
     "day-list__item--selected": props.selected,
     "day-list__item--full": props.spots === 0
@@ -21,7 +20,7 @@ export default function DayListItem(props) {
   };
 
   return (
-    <li className={dayClass} onClick={() => props.setDay(props.name)}>
+    <li className={dayClass} onClick={props.setDay} selected={props.selected}>
       <h2 className="text--regular">{props.name}</h2>
       <h3 className="text--light">{formatSpots()}</h3>
     </li>
