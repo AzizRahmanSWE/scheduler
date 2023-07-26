3f0e1e5aae561cd87c71474366deb8b77e3bc87d
Friies <bahram.parsa2019@gmail.com>
2023-07-26 16:21:55 -0400

    Create Form.test.js and tests for it. - Add validation fn to Form.js to prevent default values for student and interviewer.

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -5,7 +5,22 @@ import Button from "components/Button";
 export default function Form(props) {
   const [student, setStudent] = useState(props.student || "");
   const [interviewer, setInterviewer] = useState(props.interviewer || null);
-  // const [error, setError] = useState("");
+  const [error, setError] = useState("");
+
+
+  function validation() {
+    if (student === '') {
+      setError("Student name cannot be blank");
+      return;
+    }
+
+    if (interviewer === null) {
+      setError("please select an interviewer");
+      return;
+    }
+    setError("");
+    props.onSave(student, interviewer);
+  }
 
   const reset = () => {
     setStudent("");
@@ -17,11 +32,10 @@ export default function Form(props) {
     props.onCancel();
   };
 
-  const save = (event) => {
-    event.preventDefault();
-    props.onSave(student, interviewer)
-  }
-
+  // const save = (event) => {
+  //   event.preventDefault();
+  //   props.onSave(student, interviewer)
+  // };
 
   return (
     <main className="appointment__card appointment__card--create">
@@ -34,10 +48,10 @@ export default function Form(props) {
             placeholder="Enter Student Name"
             value={student}
             onChange={(event) => setStudent(event.target.value)}
+            data-testid="student-name-input"
           />
+        <section className="appointment__validation">{error}</section>
         </form>
-      {/* <section className="appointment__validation">{error}</section> */}
-
         <InterviewerList 
           interviewers={props.interviewers} 
           value={interviewer}
@@ -47,7 +61,7 @@ export default function Form(props) {
       <section className="appointment__card-right">
         <section className="appointment__actions">
           <Button danger onClick={cancel}>Cancel</Button>
-          <Button confirm onClick={save}>Save</Button>
+          <Button confirm onClick={event => validation()}>Save</Button>
         </section>
       </section>
     </main>
