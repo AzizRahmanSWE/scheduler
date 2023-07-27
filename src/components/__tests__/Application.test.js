e06374221a121571d579325ab4d45862f58ba80f
Friies <bahram.parsa2019@gmail.com>
2023-07-27 16:31:23 -0400

    Add debug when rendering application. - Add more test. - Use queryByText to find the absence of an element.

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -9,6 +9,7 @@ import {
   getByAltText,
   getByPlaceholderText,
   getByText,
+  queryByText,
 } from "@testing-library/react"; 
 import Application from "components/Application";
 
@@ -27,7 +28,7 @@ describe("Application", () => {
   });
 
   it("loads data, books an interview and reduces the spots remaining for the first day by 1", async () => {
-    const { container } = render(<Application />);
+    const { container, debug } = render(<Application />);
     
     await waitForElement(() => getByText(container, "Archie Cohen"));
     
@@ -39,8 +40,17 @@ describe("Application", () => {
     fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
       target: { value: "Lydia Miller-Jones" },
     });
+    
     fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
     fireEvent.click(getByText(appointment, "Save"));
 
+    expect(getByText(appointment, "Saving...")).toBeInTheDocument();
+    
+    await waitForElement(() => getByText(appointment, "Lydia Miller-Jones"));
+    
+    const day = getAllByTestId(container, "day").find(day => queryByText(day, "Monday")
+    );
+
+    expect(getByText(day, "no spots remaining")).toBeInTheDocument();
   });
 })
